37e5af17909df1a98928abe298365b6a38b67a32
Yaty <yaty@live.fr>
2017-07-27 02:08:44 +0200

    Backend URL

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BACKEND: '"http://localhost:8081/api/"'
+  BACKEND: '"http://localhost:8081/"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   NODE_ENV: '"production"',
-  BACKEND: '"/api/"'
+  BACKEND: '"/"'
 }
