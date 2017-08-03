9686e40d9c2763566a71cb257eaed01107ae1231
Yaty <yaty@live.fr>
2017-08-04 00:05:23 +0200

    New Vue components, will be use to handle members statistics from the manager POV

---
+++ client/views/stats/manager/Members.vue
@@ -0,0 +1,17 @@
+<!--
+Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
+Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
+-->
+
+<template>
+  <div>
+    Members
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
