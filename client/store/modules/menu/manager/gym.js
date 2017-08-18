4cdd1dab800ce87709b90dd6c5c74b7fd438d79e
Yaty <yaty@live.fr>
2017-08-18 18:10:06 +0200

    Adding gym parameters pages + routes

---
+++ client/store/modules/menu/manager/gym.js
@@ -0,0 +1,32 @@
+/*
+Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
+Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
+*/
+
+import lazyLoading from '../lazyLoading'
+
+export default {
+  path: '/gym-management',
+  meta: {
+    icon: 'fa-gear',
+    expanded: false,
+    label: 'My gym',
+    auth: 'owner'
+  },
+  component: lazyLoading('owners/gym', true),
+  redirect: 'gym-management/parameters',
+
+  children: [
+    {
+      name: 'Gym parameters',
+      path: 'parameters',
+      component: lazyLoading('owners/gym/Parameters'),
+      meta: {
+        label: 'Parameters'
+      }
+    }
+  ]
+}
--- client/views/owners/gym/Parameters.vue
+++ client/views/owners/gym/Parameters.vue
@@ -1,13 +1,17 @@
-/*
+<!--
 Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
 Unauthorized copying of this file, via any medium is strictly prohibited
 Proprietary and confidential
 Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
 Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
-*/
+-->
 
-import members from './members'
-import stats from './stats'
-import gym from './gym'
+<template>
+  <div>
+    Parameters
+  </div>
+</template>
 
-export default { gym, members, stats }
+<script></script>
+
+<style scoped></style>
