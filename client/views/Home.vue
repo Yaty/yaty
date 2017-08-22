469167d897c5f28477b3c7aaef3b1d01d42a246c
Yaty <yaty@live.fr>
2017-08-22 19:55:10 +0200

    New desc

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -9,40 +9,28 @@ Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
 <template>
   <div class="content has-text-centered">
     <p>
-      <img width="200" src="~assets/logo.svg" :alt="projectdescription">
+      <img width="200" src="~assets/logo.svg" :alt="description">
     </p>
 
     <h1 class="is-title is-bold">{{ name.replace('-', ' ') }}</h1>
-
     <p>
-      <strong>{{ description }}</strong>
-    </p>
-
-    <p>
-      TMP :
-      <strong>2 {{ user }}</strong>
+      Managing your gym has never been that easy !
     </p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  export default {
 
-export default {
+    data () {
+      return this.$store.state.pkg
+    }
 
-  data () {
-    return this.$store.state.pkg
-  },
-
-  computed: mapGetters({
-    user: 'user'
-  })
-
-}
+  }
 </script>
 
 <style lang="scss" scoped>
-.is-title {
-  text-transform: capitalize;
-}
+  .is-title {
+    text-transform: capitalize;
+  }
 </style>
