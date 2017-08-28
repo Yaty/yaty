5ca66611bfc1f4703d50a21e4424e457b9133c34
Yaty <yaty@live.fr>
2017-08-28 12:45:21 +0200

    Validation message and type method

--- client/views/owners/management/Add.vue
+++ client/views/owners/management/Add.vue
@@ -10,115 +10,121 @@ Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
   <div class="box">
     <b-loading :active.sync="loading" :canCancel="true"></b-loading>
     <p class="title">{{ user.selectedGym.name }}</p>
-    <div class="columns is-multiline is-centered">
-      <div class="column is-full has-text-centered" v-if="error">{{ error }}</div>
+    <div v-if="ready">
+      <div class="columns is-multiline is-centered">
+        <div class="column is-full has-text-centered" v-if="error">{{ error }}</div>
 
-      <div class="column is-full">
-        <b-table
-          :data="members"
-          striped
-          narrowed
-          mobile-cards
-        >
-          <template scope="props">
-            <b-table-column field="created" label="Created" :visible="creationDone" sortable centered>
-              <b-field>
-                <b-checkbox v-model="props.row.created" disabled></b-checkbox>
-              </b-field>
-            </b-table-column>
+        <div class="column is-full">
+          <b-table
+            :data="members"
+            striped
+            narrowed
+            mobile-cards
+          >
+            <template scope="props">
+              <b-table-column field="created" label="Created" :visible="creationDone" sortable centered>
+                <b-field>
+                  <b-checkbox v-model="props.row.created" disabled></b-checkbox>
+                </b-field>
+              </b-table-column>
 
-            <b-table-column field="email" label="E-mail" sortable centered>
-              <b-field>
-                <b-input v-model="props.row.email" type="email" placeholder="email@example.org"></b-input>
-              </b-field>
-            </b-table-column>
+              <b-table-column field="email" label="E-mail" sortable centered>
+                <b-field :type="getType($v.members.$each[props.index].email.$invalid)" :message="getMessage('email', props.index)">
+                  <b-input v-model="props.row.email" type="email" placeholder="email@example.org"></b-input>
+                </b-field>
+              </b-table-column>
 
-            <b-table-column field="name" label="First Name" sortable centered>
-              <b-field>
-                <b-input v-model="props.row.name" type="text" placeholder="Name"></b-input>
-              </b-field>
-            </b-table-column>
+              <b-table-column field="name" label="First Name" sortable centered>
+                <b-field :type="getType($v.members.$each[props.index].name.$invalid)" :message="getMessage('name', props.index)">
+                  <b-input v-model="props.row.name" type="text" placeholder="Name"></b-input>
+                </b-field>
+              </b-table-column>
 
-            <b-table-column field="lastname" label="Last Name" sortable centered>
-              <b-field>
-                <b-input v-model="props.row.lastname" type="text" placeholder="Lastname"></b-input>
-              </b-field>
-            </b-table-column>
+              <b-table-column field="lastname" label="Last Name" sortable centered>
+                <b-field :type="getType($v.members.$each[props.index].lastname.$invalid)" :message="getMessage('lastname', props.index)">
+                  <b-input v-model="props.row.lastname" type="text" placeholder="Last Name"></b-input>
+                </b-field>
+              </b-table-column>
 
-            <b-table-column field="subscription" label="Subscription" sortable centered>
-              <b-field>
-                <b-select v-model="props.row.subscription" class="has-text-centered" placeholder="Select a subscription">
-                  <option v-for="subscription in subscriptions" :value="subscription">{{ subscription.label }}</option>
-                </b-select>
-              </b-field>
-            </b-table-column>
+              <b-table-column field="subscription" label="Subscription" sortable centered>
+                <b-field :type="getType($v.members.$each[props.index].subscription.$invalid)" :message="getMessage('subscription', props.index)">
+                  <b-select v-model="props.row.subscription" class="has-text-centered" placeholder="Subscription">
+                    <option v-for="subscription in subscriptions" :value="subscription">{{ subscription.label }}</option>
+                  </b-select>
+                </b-field>
+              </b-table-column>
 
-            <b-table-column field="role" label="Role" sortable centered>
-              <b-field>
-                <b-select v-model="props.row.role" class="has-text-centered">
-                  <option v-for="role in roles.roles" :value="role">{{ role.charAt(0).toUpperCase() + role.slice(1) }}</option>
-                </b-select>
-              </b-field>
-            </b-table-column>
-          </template>
+              <b-table-column field="role" label="Role" sortable centered>
+                <b-field :type="getType($v.members.$each[props.index].role.$invalid)" :message="getMessage('role', props.index)">
+                  <b-select v-model="props.row.role" class="has-text-centered">
+                    <option v-for="role in roles.roles" :value="role">{{ role.charAt(0).toUpperCase() + role.slice(1) }}</option>
+                  </b-select>
+                </b-field>
+              </b-table-column>
+            </template>
 
-          <div slot="empty" class="has-text-centered">
-            There is currently no members to display.
-          </div>
-        </b-table>
-      </div>
+            <div slot="empty" class="has-text-centered">
+              There is currently no members to display.
+            </div>
+          </b-table>
+        </div>
 
-      <div class="column has-text-centered is-full">
-        <button class="button is-primary is-outlined" @click="addMember">New member</button>
-        <button class="button is-info is-outlined" :title="createMembersBtnTitle" :disabled="!checkIfMembersAreCorrect" @click="createMembers">Create members</button>
+        <div class="column has-text-centered is-full">
+          <button class="button is-primary is-outlined" @click="addMember">New member</button>
+          <button class="button is-info is-outlined" :title="$v.$invalid ? 'Check your inputs' : ''" :disabled="$v.$invalid" @click="createMembers">Create members</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  // TODO : Use a tool to validate data
   import { mapGetters } from 'vuex'
+  import { required, requiredIf, minLength, email, maxLength } from 'vuelidate/lib/validators'
 
   export default {
     data () {
       return {
         error: null,
         loading: null,
+        ready: null,
         creationDone: null,
         roles: null,
         subscriptions: null,
-        members: [],
-        createMembersBtnTitle: ''
+        members: []
+      }
+    },
+    validations: {
+      members: {
+        requiredIf: requiredIf('ready'),
+        $each: {
+          'email': {
+            required,
+            email
+          },
+          name: {
+            required,
+            maxLength: maxLength(45),
+            minLength: minLength(2)
+          },
+          lastname: {
+            required,
+            maxLength: maxLength(45),
+            minLength: minLength(2)
+          },
+          subscription: {
+            required
+          },
+          role: {
+            required
+          }
+        }
       }
     },
     computed: {
       ...mapGetters({
         user: 'user'
-      }),
-      // TODO : Check if this email was already entered
-      checkIfMembersAreCorrect () {
-        if (!this.subscriptions || this.subscriptions.length === 0 || this.members.length === 0) {
-          this.createMembersBtnTitle = 'Some of your members are invalid'
-          return false
-        }
-        let validity = true
-        for (let i = 0; i < this.members.length; i++) {
-          const member = this.members[i]
-          const nameIsInvalid = !member.name || member.name.length === 0
-          const lastNameIsInvalid = !member.lastname || member.lastname.length === 0
-          const emailIsInvalid = !member.email || member.email.length === 0 || !this.isMailValid(member.email)
-          const subscriptionIsInvalid = !member.subscription || !member.subscription.id
-          if (nameIsInvalid || lastNameIsInvalid || emailIsInvalid || subscriptionIsInvalid) {
-            member.valid = false
-            validity = false
-          } else {
-            member.valid = true
-          }
-        }
-        if (!validity) this.createMembersBtnTitle = 'Some of your members are invalid'
-        return validity
-      }
+      })
     },
     created () {
       this.loading = true
@@ -127,6 +133,7 @@ Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
         .then(() => {
           this.addMember()
           this.loading = false
+          this.ready = true
         })
         .catch(e => {
           if (e.hasOwnProperty('response')) this.error = e.response.statusText
@@ -135,8 +142,46 @@ Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
         })
     },
     methods: {
-      isMailValid (mail) {
-        return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(mail)
+      /**
+       * This function take a field name and it's index inside the members array
+       * It look into validation to see if some fields are valid or not
+       * If some fields are invalid then it will return an appropriate message
+       * @param fieldName The key of the field
+       * @param index The position in the members array
+       * @returns {string}
+       */
+      getMessage (fieldName, index) {
+        if (!this.$v.members.$each[index].$invalid) return '' // If it's valid there is no need for a message
+
+        const validationStatus = this.$v.members.$each[index][fieldName]
+        const validations = Object.keys(validationStatus)
+        let message = ''
+
+        for (let i = 0; i < validations.length; i++) {
+          let validation = validations[i]
+          if (!validationStatus[validation] && validation.charAt(0) !== '$') {
+            switch (validation) {
+              case 'required':
+                message = 'This field is required.'
+                break
+              case 'email':
+                message = 'This is not a correct email.'
+                break
+              case 'maxLength':
+                message = 'This field cannot have more than ' + validationStatus.$params.maxLength.max + ' characters.'
+                break
+              case 'minLength':
+                message = 'This field cannot have less than ' + validationStatus.$params.minLength.min + ' characters.'
+                break
+            }
+            if (message.length > 0) return message
+          }
+        }
+
+        return message
+      },
+      getType (fieldIsInvalid) {
+        return fieldIsInvalid ? 'is-danger' : ''
       },
       fetchRoles () {
         return new Promise((resolve, reject) => {
